ade5c5b99f168520a88a23f59b1b8bf1c1b3b7ab
Thomas <123500706+quikstart86@users.noreply.github.com>
2023-12-16 21:08:04 +0000

    added in const variables for the password length, and confir mcharagters used in password

--- script.js
+++ script.js
@@ -42,21 +42,27 @@
 
 // Array of special characters to be included in password
 const specialCharacters = ['@', '%', '+', '\\', '/', "'", '!', '#', '$', '^', '?', ':', ',', ')', '(', '}', '{', ']', '[', '~', '-', '_', '.', ``];
-
 // Array of numeric characters to be included in password
 const numericCharacters = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
-
 // Array of lowercase characters to be included in password
 const lowerCasedCharacters = [
     'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
-
 // Array of uppercase characters to be included in password
 const upperCasedCharacters = [
     'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
 
+    
+    const confirmLength = "";
+    const confirmSpecialCharacters;
+    const confirmNumericCharacters;
+const confirmLowerCasedCharacters;
+const confirmUpperCasedCharacters;
+
+
 // Function to prompt user for password options
 function getPasswordOptions() {
-
+    const confirmLength = (prompt(`Please enter a valid number between 8 and 128 for the password`));
+alert(`Please enter a valid number between 8 and 128 for the password.`)
 }
 
 // Function for getting a random element from an array
